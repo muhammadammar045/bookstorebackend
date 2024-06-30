ec7ad369fdafd18126d79239b3880dcb84c1a66b
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-30 15:28:07 +0500

    added absolute path

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "/public/temp") // the destination of file
+        cb(null, "public/temp") // the destination of file
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname) // the file name to be stored in destination
