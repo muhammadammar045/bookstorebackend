2db1a5f7034d18f23e3a7fb04b0ff4aaa12f06ca
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-30 17:11:41 +0500

    multer adjusted

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,21 @@
-import multer from "multer"
+// multer.js
+
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp") // the destination of file
+        const uploadPath = path.join(process.cwd(), 'public', 'temp'); // Adjusted for Vercel deployment
+
+        // Ensure directory exists, create if not
+        fs.mkdirSync(uploadPath, { recursive: true });
+
+        cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname) // the file name to be stored in destination
+        cb(null, file.originalname);
     }
-})
+});
 
-export const uploadOnMulter = multer(
-    {
-        storage
-    }
-)
+export const uploadOnMulter = multer({ storage });
