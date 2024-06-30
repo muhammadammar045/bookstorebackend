9a01bcdac8c4104d5a4ea544de774448f3e1e4c9
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-30 16:46:35 +0500

    added absolute path

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,27 +1,16 @@
-import multer from "multer";
-import fs from "fs";
-import path from "path";
-
-// Resolve the __dirname
-const __dirname = path.resolve();
-
-// Define the upload directory
-const uploadDir = path.join(__dirname, "public", "temp");
-
-// Ensure the directory exists
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+import multer from "multer"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir); // Use absolute path
+        cb(null, "./public/temp") // the destination of file
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // The file name to be stored in the destination
+        cb(null, file.originalname) // the file name to be stored in destination
     }
-});
+})
 
-export const uploadOnMulter = multer({
-    storage
-});
+export const uploadOnMulter = multer(
+    {
+        storage
+    }
+)
