bd46272c6540a15d2d6e29c846a8e5ede707a14a
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-06 09:40:02 +0500

    pagination middleware updated

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -55,23 +55,6 @@ const getAllBooks = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUserBooks = asyncHandler(async (req, res) => {
-    // const userId = req.user?._id
-
-    // if (!userId) {
-    //     throw new ApiError(400, "userId is Required");
-    // }
-
-    // const books = await Books.find({ author: userId });
-
-    // if (!books) {
-    //     throw new ApiError(404, "No books found");
-    // }
-
-    // return res
-    //     .status(200)
-    //     .json(
-    //         new ApiResponse(200, books, "User Books fetched successfully")
-    //     );
 
     const paginatedResult = res.paginatedResult;
 
